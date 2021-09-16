96fb8894060a15b52405773fee5c3b65e246f0d7
LilianeH-L <90001226+LilianeH-L@users.noreply.github.com>
2021-09-16 10:38:05 -0400

    Version finale

--- exercice1.py
+++ exercice1.py
@@ -1,8 +1,16 @@
 def fizzBuzz(n):
-    # TODO imprimer la chaine de caractère appropriée avec la fonction print().
+    #    TODO imprimer la chaine de caractère appropriée avec la fonction print().
     #  Assigner ensuite la valeur à la variable resultat
 
-    resultat = asdfasd
+    if (n % 3 == 0 and n % 5 == 0):
+        resultat = "fizzbuzz"
+
+    elif (n % 3 == 0):
+        resultat = "fizz"
+    elif(n % 5 == 0):
+        resultat = "buzz"
+    else:
+        resultat = n
     return resultat
 
 if __name__ == '__main__':
--- exercice6.py
+++ exercice6.py
@@ -8,7 +8,9 @@ def trouverAngle(nombreComplexe):
 
 def trouverModule(nombreComplexe):
     # TODO: Calculer le module du nombre complexe et l'assigner dans "module"
-    module =
+    a=nombreComplexe.real
+    b=nombreComplexe.imag
+    module = math.sqrt(a**2+b**2)
 
     return module
 
@@ -20,23 +22,24 @@ def effectuerRotation(nombreComplexe, angle_rotation, trouverModule):
     angle = trouverAngle(nombreComplexe)
 
     # TODO: Afficher le module et l'angle du nombre complexe (3 decimales de précision)
-
+    print(f"Le module est {round(module,3)}, l'angle est {round(angle,3)}")
 
     # TODO: Calculer le nouveau nombre complexe après rotation, assigner le nouveau nombre complexe à la variable 'resultat'
-
-    resultat =
+    vect_rotation= complex(math.cos(math.radians(angle_rotation)), math.sin(math.radians(angle_rotation)))
+    resultat = nombreComplexe * vect_rotation
 
     nouveauModule = trouverModule(resultat)
     nouvelAngle = trouverAngle(resultat)
 
     # TODO : Afficher le nouveau module et le nouvel angle du nombre complexe après rotation (3 decimales de précision)
-
+    print(f"Le nouveau module est {round(nouveauModule,3)}, le nouvel angle est {round(nouvelAngle,3)}")
     return resultat
 
 
 def dessiner(number, label):
+    ax = plt.subplot(projection='polar')
     if number != None:
-        plt.polar([0, math.radians(trouverAngle(number))], [0, trouverModule(number)], marker='o', label=label)
+        ax.plot([0, math.radians(trouverAngle(number))], [0, trouverModule(number)], marker='o', label=label)
 
 if __name__ == '__main__':
     nombre = complex(input("Veuillez entrer un nombre complexe de votre choix sous la forme a+bj (exemple: 1+2j): "))
